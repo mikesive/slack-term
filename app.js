3ecf8c5c9ba99de273ead0fda022280746ae8274
Michael Sive <mikesive@gmail.com>
2015-12-09 22:20:58 +1000

    add json body parser

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
-
+app.use(bodyParser.json());
 // Adds a new remote server
 app.post('/remote', function (req, res) {
   // var remoteName = req.query.text;
   // var remoteHost = req.query.remoteHost;
-  console.log('Request received...\ndata: ' + req.param('text'));
-  res.send('POST request data: ' + req.param('text'));
+  console.log('Request received...\ndata: ' + req.body.text);
+  res.send('POST request data: ' + req.body.text + "\n");
 });
 
 console.log('listening...');
