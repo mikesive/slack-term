5fab1446ef6dca5a97deb5518e1372aeda649397
Michael Sive <mikesive@gmail.com>
2015-12-09 21:58:54 +1000

    return data

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var app = express();
 
 // Adds a new remote server
 app.post('/remote', function (req, res) {
-  var remoteName = req.query.remoteName;
+  var remoteName = req.query.text;
   var remoteHost = req.query.remoteHost;
-  console.log('Request received...');
+  console.log('Request received...\ndata: ' + text);
   res.send('POST request to the homepage\n');
 });
 
 console.log('listening...');
-app.listen(80);
+app.listen(9000);
