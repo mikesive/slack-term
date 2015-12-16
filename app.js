0c531fe4862f9880b131912a374eeb9feaeca8e6
Michael Sive <mikesive@gmail.com>
2015-12-16 17:45:42 +1000

    debugging

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/remote', function (req, res) {
   // var remoteName = req.query.text;
   // var remoteHost = req.query.remoteHost;
-  console.log('Request received...\ndata: ' + JSON.stringify(req, null, 4));
-  res.send('POST request data: ' + JSON.stringify(req, null, 4) + "\n");
+  console.log('Request received...\nreq: ' + req);
+  console.log('Request received...\nparams: ' + req.params);
+  console.log('Request received...\nquery: ' + req.query);
+    console.log('Request received...\nbody: ' + req.body);
+  res.send('POST request data: ' + req.body + "\n");
 });
 
 console.log('listening...');
