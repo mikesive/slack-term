460b4444603cdba686bb306cda1971ed75b7f7d8
Michael Sive <mikesive@gmail.com>
2015-12-16 17:49:20 +1000

    debugging

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.post('/remote', function (req, res) {
   // var remoteName = req.query.text;
   // var remoteHost = req.query.remoteHost;
   console.log('Request received...\nreq: ' + req);
-  console.log('Request received...\nparams: ' + req.params);
-  console.log('Request received...\nquery: ' + req.query);
-    console.log('Request received...\nbody: ' + req.body);
-  res.send('POST request data: ' + req.body + "\n");
+  console.log('Request received...\nparams: ' + JSON.stringify(req.params));
+  console.log('Request received...\nquery: ' + JSON.stringify(req.query));
+  console.log('Request received...\nbody: ' + JSON.stringify(req.body));
+  res.send('POST request data: ' + JSON.stringify(req.body) + "\n");
 });
 
 console.log('listening...');
