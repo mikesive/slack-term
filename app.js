931c10f75649eaf3d4ca540551a28d57212279ce
Michael Sive <mikesive@gmail.com>
2015-12-16 17:17:57 +1000

    debugging params

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 var app = express();
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // Adds a new remote server
 app.post('/remote', function (req, res) {
+  debugger;
   // var remoteName = req.query.text;
   // var remoteHost = req.query.remoteHost;
   console.log('Request received...\ndata: ' + JSON.stringify(req.query.text));
