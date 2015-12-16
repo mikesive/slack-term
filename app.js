5acc1d79f58edbcd211e1c793ad2dc893a67d608
Michael Sive <mikesive@gmail.com>
2015-12-16 18:07:45 +1000

    debugging

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('./services/commandSet');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -7,7 +8,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/remote', function (req, res) {
   // var remoteName = req.query.text;
   // var remoteHost = req.query.remoteHost;
-  res.send('POST request data: ' + req.body.text + "\n");
+
+  requestData = req.body;
+  commands = requestData.text;
+  user_id = requestData.user_id;
+  var commandSet = new CommandSet(commands);
+  var errors = commandSet.getErrors();
+  if (errors.length() > 0){
+    res.send(errors.join("\n"));
+  }
+  else {
+    res.send('POST request data: ' + req.body.text + "\n");
+  }
 });
 
 console.log('listening...');
---
+++ services/commandSet.js
@@ -0,0 +1,22 @@
+module.exports = {
+  CommandSet: function(commands){
+    this.commands = commands;
+    this.errors = [];
+    this.checkErrors();
+
+
+    this.checkErrors = function(){
+      if (!this.testNewLine()){
+        this.errors.push("Please start your commands with a newline character (Shift + Enter in Slack).");
+      }
+    };
+
+    this.getErrors = function(){
+      return this.errors;
+    };
+
+    this.testNewLine = function(){
+      return this.commands.startsWith("\n");
+    };
+  }
+};
