8e31eaa843a8e2c3de9824a7dca63bd3678e6785
Michael Sive <mikesive@gmail.com>
2016-01-11 17:18:04 +1000

    add ssl

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var https = require('https');
+var http = require('http');
 var bodyParser = require('body-parser');
 
 // Services
@@ -7,6 +9,11 @@ var SubRouter = require('./services/subRouter');
 // Set up server
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Default server test
+app.get('/', function(req, res){
+  res.send('Success! Server is up.');
+});
 
 // Route for Slack post
 app.post('/remote', function (req, res) {
@@ -29,5 +36,13 @@ app.post('/remote', function (req, res) {
 });
 
 var port = process.env.PORT || 3000;
+var httpsPort = process.env.HTTPSPORT || 443;
 console.log('Listening on '+ port + '...');
-app.listen(port);
+http.createServer(app).listen(port);
+
+var credentials = {
+  key: fs.readFileSync('/etc/letsencrypt/live/sshlack.sive.com.au/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/sshlack.sive.com.au/fullchain.pem'),
+  ca: fs.readFileSync('/etc/letsencrypt/live/sshlack.sive.com.au/chain.pem')
+};
+https.createServer(credentials, app).listen(httpsPort);
